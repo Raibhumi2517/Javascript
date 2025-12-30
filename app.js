c90ab0bb4cc940176e12eea7dcdbd015111c8dda
Binay Kumar <binayray7766@@gmail.com>
2025-12-30 17:22:49 +0530

    completed 1st project

---
+++ app.js
@@ -0,0 +1,25 @@
+// let color ="yellow";
+
+// if(color==="red") {
+
+//     console.log("stop:light color is red")
+// }
+// if(color==="yellow"){
+
+// console.log("wait: light color is yellow")
+// }
+// if(color==="green"){
+
+//     console.log("go: light color is green")
+// }
+
+let age =14;
+if(age>=18){
+    console.log("you can vote");
+}
+else if (age>=18) {
+    console.log("you cannot vote2");
+}
+else if (age < 18) {
+    console.log("you cannot vote2");
+}
