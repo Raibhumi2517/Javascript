3afaf04a69c73f3baa0d8d2f2eddb1464d2bf608
Binay Kumar <binayray7766@@gmail.com>
2025-12-31 13:56:30 +0530

    else if else

---
+++ PracticeQS1.js
@@ -0,0 +1,26 @@
+// practice Qs
+// A systen to calculate popcorn prices based on the size customer asked for:
+// if size is 'XL',price is RTCRtpScriptTransform. 250
+// if size is 'L',price is 200
+// if size is 'M',price is Rs 100
+// if Size is 's', price is RS.50
+let size='S';
+
+if(size==="XL")
+{
+    console.log("Price is RS.250");
+}
+else if(size==="L")
+{
+    console.log("Price is RS.200");
+}
+
+else if(size==="M")
+{
+    console.log("Price is RS.100");
+}
+
+else(size==="S")
+{
+    console.log("Price is RS 50");
+}
