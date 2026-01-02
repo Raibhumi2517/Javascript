6f6ea6aeb3f1a76df4a09922b835cdc0f6042358
Binay Kumar <binayray7766@@gmail.com>
2026-01-02 11:53:38 +0530

    javascript assign

---
+++ assign1.js
@@ -0,0 +1,52 @@
+// let num =10;
+// if(num%10===0){
+//     console.log("good");
+// }
+// else {
+//     console.log("bad");
+// }
+// let userName =prompt("please enter your name");
+// let age=prompt("enter your age");
+// alert(`${userName} is ${age} years old.`);
+// let quater=4;
+// switch(quater){
+//         case  1 :
+//         console.log("january,february,March");
+//         break;
+//         case 2 :
+//         console.log("April");
+//         break;
+//         case 3 :
+//         console.log("july,August,September");
+//         break;
+//         case 4 :
+//         console.log("October,November,December");
+//         break;
+//         default: console,log("NOT A QUARTER!");
+// }
+
+
+// let str="Aeroplane";
+// if ((str[0]=="A" || str[0]=="a") && (str.length>5)){
+//     console.log("Golden String");
+// }
+// else{
+//     console.log("Not a golden string")
+// }
+ //write a program to find the largest of three numbers
+// let a=45;
+// let b=45;
+// let c=7;
+// if(a>=b && a>=c)
+// {
+//     console.log("a is greater");
+// }
+// if(b>=a && b>=c)
+// {
+//     console.log("b is greater");
+// }
+// if(c>=a && c>=b)
+// {
+//     console.log("c is greater");
+// }
+// String Methods with Arguments
